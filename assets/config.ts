9e4c01a1ec111b2467cc80f2fb556e5e06fa1d6a
ando-dev <emal@exemple.com>
2024-07-04 13:35:26 +0200

    ..

--- assets/config.ts
+++ assets/config.ts
@@ -2,4 +2,9 @@ export class Config {
     "url": {
         apiUrl: string;
     };
+    "users": [
+        {id:string,mdp:string,role:string},
+        {id:string,mdp:string,role:string},
+        {id:string,mdp:string,role:string}
+    ]
 }
