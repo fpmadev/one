6be5ac968d4fcfa8874f9b8b4044429ef2a86058
ando-dev <77fpmamelun@gmail.com>
2024-07-10 14:20:40 +0200

    ..

---
+++ assets/config.ts
@@ -0,0 +1,10 @@
+export class Config {
+    "url": {
+        apiUrl: string;
+    };
+    "users": [
+        {id:string,mdp:string,role:string},
+        {id:string,mdp:string,role:string},
+        {id:string,mdp:string,role:string}
+    ]
+}
